20d3a94a150335aec3c99b075ea830960d6fe705
Xingwei Ruan <xingweiruan@gmail.com>
2015-04-28 02:02:02 +0800

    compatible fix

--- src/com/seventh7/mybatis/util/MapperUtils.java
+++ src/com/seventh7/mybatis/util/MapperUtils.java
@@ -10,6 +10,7 @@ import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.ide.fileTemplates.FileTemplateUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
